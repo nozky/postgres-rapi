3e21ff7274dbc811c92954f22b8f8cb6050c8f9f
nozky <norvillie.villaruel@edu.sait.ca>
2022-11-06 02:22:53 -0700

    added serving html

--- index.ts
+++ index.ts
@@ -10,6 +10,15 @@ app.use(express.urlencoded({ extended: true} ))
 app.use('/user', user)
 app.use('/post', post)
 
+
+app.get('/', (req, res) => {
+  res.sendFile( __dirname +  "/public/index.html")
+}) 
+
+app.get('/error', (req, res) => {
+  res.sendFile(__dirname + '/public/error.html')
+})
+
 app.listen( 3000, ()=> {
   console.log(`Listening on port 3000`)
 })

6f4a1cab99cf8bce6aa5df1be42d469fb56fece6
nozky <norvillie.villaruel@edu.sait.ca>
2022-11-06 02:36:38 -0700

    switch to express.static to serve html files

--- index.ts
+++ index.ts
@@ -7,17 +7,10 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true} ))
 
+app.use(express.static('public'))
 app.use('/user', user)
 app.use('/post', post)
 
-
-app.get('/', (req, res) => {
-  res.sendFile( __dirname +  "/public/index.html")
-}) 
-
-app.get('/error', (req, res) => {
-  res.sendFile(__dirname + '/public/error.html')
-})
 
 app.listen( 3000, ()=> {
   console.log(`Listening on port 3000`)
